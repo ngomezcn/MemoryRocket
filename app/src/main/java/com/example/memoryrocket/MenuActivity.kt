ba4f5a090dbb8136988c1bb42e820dc1d6533a8b
ngomez <naimgomezcn@gmail.com>
2022-10-04 17:32:26 +0200

    d

---
+++ app/src/main/java/com/example/memoryrocket/MenuActivity.kt
@@ -0,0 +1,11 @@
+package com.example.memoryrocket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
