ec5899f160959d147b59771e1f1372d57d7be00f
naimgomezcn@gmail.com <naimgomezcn@gmail.com>
2022-10-05 19:59:49 +0200

    d

---
+++ app/src/main/java/com/example/memoryrocket/score_screen.kt
@@ -0,0 +1,11 @@
+package com.example.memoryrocket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class score_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_score_screen)
+    }
+}
