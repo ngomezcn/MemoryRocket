ecd33f75d8a90f3ab30c709667f6c062fcfa2abb
Naïm Gómez <castelldeneim@gmail.com>
2022-10-12 14:05:41 +0200

    game activity landscape design

--- app/src/main/java/com/example/memoryrocket/MenuActivity.kt
+++ app/src/main/java/com/example/memoryrocket/MenuActivity.kt
@@ -1,10 +1,14 @@
 package com.example.memoryrocket
 
+import android.R
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
 import com.example.memoryrocket.databinding.ActivityMenuBinding
+
 
 class MenuActivity : AppCompatActivity() {
     lateinit var binding: ActivityMenuBinding
@@ -18,6 +22,15 @@ class MenuActivity : AppCompatActivity() {
             val intent = Intent(this, GameActivity::class.java)
             startActivity(intent)
         }
+
+
+        val spinner = binding.btnDifficultySelector
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.phoneTypes, R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
 
     }
 }
