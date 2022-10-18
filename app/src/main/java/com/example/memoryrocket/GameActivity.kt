09abe6c27a1c2e2107ba34a5d07faff36c19f036
naimgomezcn@gmail.com <naimgomezcn@gmail.com>
2022-10-18 19:22:11 +0200

    asdsad

--- app/src/main/java/com/example/memoryrocket/GameActivity.kt
+++ app/src/main/java/com/example/memoryrocket/GameActivity.kt
@@ -1,10 +1,44 @@
 package com.example.memoryrocket
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.memoryrocket.databinding.ActivityGameBinding
-import com.example.memoryrocket.databinding.ActivityMenuBinding
+import kotlinx.coroutines.*
+import kotlin.system.*
+
+
+class Card(private val imgView : ImageView?, private val imgDrawable: Int, val gameActivity: GameActivity)  {
+
+    private val hideImg: Int = R.drawable.memory_hide
+
+    init {
+        imgView?.setOnClickListener {
+            showImage()
+        }
+
+    }
+
+    private fun showImage() {
+        imgView?.setImageResource(imgDrawable)
+    }
+
+    private fun hideImage() {
+        imgView?.setImageResource(hideImg)
+    }
+
+    fun toggleCard() {
+        showImage()
+
+        hideImg()
+    }
+}
+
+class CardPair(val image : Int, val card1: Card, val card2: Card) {
+
+
+}
 
 class GameActivity : AppCompatActivity() {
     lateinit var binding: ActivityGameBinding
@@ -16,11 +50,20 @@ class GameActivity : AppCompatActivity() {
         binding.timer.start()
 
         // Temporal per mostrar el fluix de treball
-        binding.root.setOnClickListener {
-            binding.timer.stop()
-            val intent = Intent(this, ScoreActivity::class.java)
-            startActivity(intent)
-        }
+        //val cardImageList : List<Int> = listOf(R.drawable.memory_01, R.drawable.memory_02, R.drawable.memory_03)
+
+        Card(binding.card01, R.drawable.memory_01)
+
+
 
     }
+
+
+    fun run_async() {
+        this@GameActivity.runOnUiThread {
+
+        }
+    }
+
+
 }
