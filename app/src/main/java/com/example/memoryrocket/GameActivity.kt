6605f0a114638288907beae8519e81e47d457822
Naïm Gómez <castelldeneim@gmail.com>
2022-10-19 21:43:42 +0200

    nt

--- app/src/main/java/com/example/memoryrocket/GameActivity.kt
+++ app/src/main/java/com/example/memoryrocket/GameActivity.kt
@@ -1,69 +1,105 @@
 package com.example.memoryrocket
 
+
 import android.content.Context
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import com.example.memoryrocket.databinding.ActivityGameBinding
-import kotlinx.coroutines.*
-import kotlin.system.*
+import java.util.*
 
 
-class Card(private val imgView : ImageView?, private val imgDrawable: Int, val gameActivity: GameActivity)  {
+class Card(val memoryImageView: ImageView?)  {
 
-    private val hideImg: Int = R.drawable.memory_hide
+    private val hideImage: Int = R.drawable.memory_hide
+    private lateinit var memoryImage: Drawable
 
     init {
-        imgView?.setOnClickListener {
+       /*memoryImageView?.setOnClickListener {
+            memoryImageView.setImageDrawable(hideImage.toDrawable())
             showImage()
-        }
+        }*/
+    }
 
+    fun setMemoryImage(drawable : Drawable) {
+        memoryImage = drawable
     }
 
     private fun showImage() {
-        imgView?.setImageResource(imgDrawable)
+        val transitionDrawable = TransitionDrawable(
+            memoryImageView?.let {
+                arrayOf(
+                    it.drawable,
+                    memoryImage
+                )
+            }
+        )
+        memoryImageView?.setImageDrawable(transitionDrawable)
+        transitionDrawable.startTransition(500)
     }
 
-    private fun hideImage() {
-        imgView?.setImageResource(hideImg)
-    }
+    fun hideImage() {
+        memoryImageView?.setImageResource(hideImage)
 
-    fun toggleCard() {
-        showImage()
 
-        hideImg()
     }
 }
 
-class CardPair(val image : Int, val card1: Card, val card2: Card) {
+class GameState() {
+
+}
+
+class MemoryGame(private val context: Context, private val binding: ActivityGameBinding)   {
+    private val oListCardImages: MutableList<Int> = mutableListOf(R.drawable.memory_01, R.drawable.memory_02, R.drawable.memory_03,
+                                                                  R.drawable.memory_01, R.drawable.memory_02, R.drawable.memory_03)
+
+    private val oListCards: List<Card> = listOf(Card(binding.card01),Card(binding.card02),
+                                                Card(binding.card03),Card(binding.card04),
+                                                Card(binding.card05),Card(binding.card06))
+
+
+    private fun setupGame() {
+        val random = Random()
+
+        for(card in oListCards) {
+            val index = random.nextInt(oListCardImages.size - 0) + 0
+            getDrawable(context, oListCardImages[index])?.let { card.setMemoryImage(it) }
+            oListCardImages.removeAt(index)
+        }
+    }
+
+    fun run() {
+        setupGame()
+        binding.timer.start()
+        for (card in oListCards) {
+            //if(card.memoryImageView?.id == v?.id) {
+            //    Toast.makeText(context, "ESPAÑÁ", Toast.LENGTH_LONG).show()
+           // }
+        }
+    }
 
 
 }
 
-class GameActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity(), OnClickListener {
     lateinit var binding: ActivityGameBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.timer.start()
 
-        // Temporal per mostrar el fluix de treball
-        //val cardImageList : List<Int> = listOf(R.drawable.memory_01, R.drawable.memory_02, R.drawable.memory_03)
-
-        Card(binding.card01, R.drawable.memory_01)
-
-
-
+        val memoryGame = MemoryGame(this,binding)
+        memoryGame.run()
     }
 
-
-    fun run_async() {
-        this@GameActivity.runOnUiThread {
-
-        }
+    override fun onClick(v: View?) {
+        Toast.makeText(this, "${v?.id}", Toast.LENGTH_LONG).show()
     }
-
-
 }
